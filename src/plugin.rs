6887bf6a743d2e43c943bb1a07e8f35fcb72577a
Donvlouss <donvlous0rixk@gmail.com>
2024-01-16 11:24:52 +0800

    update embedded path for different platform

--- src/plugin.rs
+++ src/plugin.rs
@@ -9,7 +9,10 @@ pub struct PointsPlugin;
 
 impl Plugin for PointsPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
+        #[cfg(all(target_family = "windows"))]
         embedded_asset!(app, "src\\", "shaders\\points.wgsl");
+        #[cfg(any(not(target_family = "windows")))]
+        embedded_asset!(app, "src/", "shaders/points.wgsl");
         app.add_plugins(MaterialPlugin::<PointsMaterial>::default());
     }
 }
